b5b34c39fb71943f0dae7d02150925030a695c2e
Park HyeonJin <hhjj9798@gmail.com>
2020-04-07 21:58:08 +0900

    3,4주차 예제&과제

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -36,7 +36,12 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ButtonsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_checkboxes) {
+            Intent intent = new Intent(this, CheckboxesActivity.class);
+            startActivity(intent);
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
